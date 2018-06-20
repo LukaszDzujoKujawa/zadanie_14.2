271c40161c06fed738550ecf7168176b9b949133
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-06-20 22:24:32 +0200

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,34 @@ var movies = [
     id: 1,
     title: 'Żywot Briana',
     desc: 'Film o życiu',
-    poster: './images/brian.jpg'
+    poster: './images/brian.jpg',
+    alt: 'Brian'
   },
   {
     id: 2,
     title: 'Król lew',
     desc: 'Film o królu sawanny',
-    poster: './images/lion.jpg'
+    poster: './images/lion.jpg',
+    alt: 'Lion king'
   },
   {
     id: 3,
     title: 'Blade Runner',
     desc: 'Film o androdidach',
-    poster: './images/blade.jpg'
+    poster: './images/blade.jpg',
+    alt: "Blade Runner"
   },
   {
     id: 4,
     title: 'Star Wars',
     desc: 'Film o lataniu w kosmosie',
-    poster: './images/starwars.jpg'
+    poster: './images/starwars.jpg',
+    alt: "Star Wars"
   }
 ];
 
 var moviesElements = movies.map(function (movie) {
-  return React.createElement('li', {key:movie.id},
+  return React.createElement('li', {key: movie.id},
     React.createElement('h2', {}, movie.title),
     React.createElement('p', {}, movie.desc),
     React.createElement('img', { src: movie.poster, width: 182})
